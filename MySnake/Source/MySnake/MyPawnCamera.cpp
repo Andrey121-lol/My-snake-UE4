7fa6dbb8dd653782e5972e61db7891f4c35240e5
Andrey Brigadir <77679107+Andrey121-lol@users.noreply.github.com>
2023-03-28 19:26:48 +0300

    CameraPawn

---
+++ MySnake/Source/MySnake/MyPawnCamera.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawnCamera.h"
+
+// Sets default values
+AMyPawnCamera::AMyPawnCamera()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	//блок для камеры
+	MyRootComponent = CreateDefaultSubobject<UBoxComponent>("RootModel");
+	RootComponent = MyRootComponent;
+	CameraSpring = CreateDefaultSubobject <USpringArmComponent>("Spring");
+	CameraSpring->SetRelativeLocation(FVector(0,0,0));
+	MyCamera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	FAttachmentTransformRules AttachmentRules(EAttachmentRule::KeepRelative, true);
+	CameraSpring->AttachToComponent(MyRootComponent,AttachmentRules);
+	MyCamera->AttachToComponent(CameraSpring,AttachmentRules);
+	
+}
+
+// Called when the game starts or when spawned
+void AMyPawnCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawnCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawnCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
